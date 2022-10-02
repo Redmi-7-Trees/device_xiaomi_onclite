6a9c57d4dd34eede586d202a0621f27acffe6815
NeZoX-star <111665604+NeZoX-star@users.noreply.github.com>
2022-10-02 17:28:55 +0300

    onclite: Welcome SparkOS!

--- spark_onclite.mk
+++ spark_onclite.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2022-2023 The SparkOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,20 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common SparkOS stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-# Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := Aditya&Ganesh
+# SpakOS Stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USE_PIXEL_CHARGER := true
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_onclite
+PRODUCT_NAME := spark_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
